aa88784b010760d4d06da20a92ea544580750173
Karl M. Davis <karl@justdavis.com>
2013-10-24 23:25:30 -0400

    Closes #6: The -console project now builds a -dist bundle.

---
+++ rps-tourney-console/dev/unpack-and-run-dist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Get the script's directory. Reference: http://stackoverflow.com/questions/59895/can-a-bash-script-tell-what-directory-its-stored-in
+scriptDirectory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Get the project directory and pull the version from the POM.
+projectDirectory=${scriptDirectory}/..
+projectVersion=`xmllint --xpath "/*[local-name()='project']/*[local-name()='parent']/*[local-name()='version']/text()" ${projectDirectory}/pom.xml`
+
+# Clean up/create the directory we'll use.
+rm -rf ${projectDirectory}/target/extracted-dist/
+mkdir ${projectDirectory}/target/extracted-dist/
+
+# Unpack the -dist bundle.
+tar --extract --ungzip --file $scriptDirectory/../target/rps-tourney-console-${projectVersion}-dist.tar.gz --directory ${projectDirectory}/target/extracted-dist/
+
+# Run the JAR from the unpacked bundle.
+java -jar $projectDirectory/target/extracted-dist/rps-tourney-console-${projectVersion}/rps-tourney-console-${projectVersion}.jar "$@"
