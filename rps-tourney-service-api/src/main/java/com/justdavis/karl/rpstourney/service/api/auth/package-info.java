4b1395c435251f146f2ce37a459cb001073f9120
Karl M. Davis <karl@justdavis.com>
2014-04-19 13:13:05 -0400

    Formatted this file.

--- rps-tourney-service-api/src/main/java/com/justdavis/karl/rpstourney/service/api/auth/package-info.java
+++ rps-tourney-service-api/src/main/java/com/justdavis/karl/rpstourney/service/api/auth/package-info.java
@@ -6,8 +6,10 @@
  */
 @XmlSchema(namespace = XmlNamespace.RPSTOURNEY_API, xmlns = { @XmlNs(prefix = "rps", namespaceURI = XmlNamespace.RPSTOURNEY_API) }, elementFormDefault = XmlNsForm.QUALIFIED)
 package com.justdavis.karl.rpstourney.service.api.auth;
+
 import javax.xml.bind.annotation.XmlNs;
 import javax.xml.bind.annotation.XmlNsForm;
 import javax.xml.bind.annotation.XmlSchema;
 
 import com.justdavis.karl.rpstourney.service.api.XmlNamespace;
+
