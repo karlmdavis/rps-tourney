330fd19a87a77b550e6522dfb5731d144e0dd048
Karl M. Davis <karl@justdavis.com>
2014-08-24 14:29:45 -0400

    ServiceAppJettyLauncher now creates a test user.
    Simplifies manual dev testing quite a bit.

--- rps-tourney-service-app/src/test/java/com/justdavis/karl/rpstourney/service/app/ServiceAppJettyLauncher.java
+++ rps-tourney-service-app/src/test/java/com/justdavis/karl/rpstourney/service/app/ServiceAppJettyLauncher.java
@@ -1,9 +1,17 @@
 package com.justdavis.karl.rpstourney.service.app;
 
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+
 import org.slf4j.bridge.SLF4JBridgeHandler;
 import org.springframework.core.env.AbstractEnvironment;
 
 import com.justdavis.karl.misc.jetty.EmbeddedServer;
+import com.justdavis.karl.rpstourney.service.api.auth.Account;
+import com.justdavis.karl.rpstourney.service.api.exceptions.UncheckedAddressException;
+import com.justdavis.karl.rpstourney.service.client.CookieStore;
+import com.justdavis.karl.rpstourney.service.client.auth.game.GameAuthClient;
+import com.justdavis.karl.rpstourney.service.client.config.ClientConfig;
 
 /**
  * Launches a Jetty {@link EmbeddedServer} to host this project's service web
@@ -31,7 +39,35 @@ public class ServiceAppJettyLauncher {
 		 * annotated Servlets, etc. available on the classpath (e.g.
 		 * GameWebApplicationInitializer), and run them.
 		 */
-		EmbeddedServer app = new EmbeddedServer(8088, false, null);
-		app.startServer();
+		EmbeddedServer server = new EmbeddedServer(8088, false, null);
+		server.startServer();
+
+		createDevUsers(server);
+	}
+
+	/**
+	 * Creates {@link Account}s, etc. for use in manual development testing.
+	 * 
+	 * @param server
+	 *            the {@link EmbeddedServer} that the web service is running in
+	 */
+	private static void createDevUsers(EmbeddedServer server) {
+		if (server == null)
+			throw new IllegalArgumentException();
+
+		// Create the web service to be used.
+		ClientConfig clientConfig = new ClientConfig(
+				server.getServerBaseAddress());
+		CookieStore cookieStore = new CookieStore();
+		GameAuthClient gameAuthClient = new GameAuthClient(clientConfig,
+				cookieStore);
+
+		// Use the client to create a login.
+		try {
+			gameAuthClient.createGameLogin(new InternetAddress(
+					"dev@rpstourney.com"), "secret");
+		} catch (AddressException e) {
+			throw new UncheckedAddressException(e);
+		}
 	}
 }
