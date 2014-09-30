963df28c6f405bedbc227f423bae98abe6d2e51a
Karl M. Davis <karl@justdavis.com>
2014-09-29 21:38:13 -0400

    Added some extra info to the comments in this script.

--- rps-tourney-deployment/deploy-to-justdavis.sh
+++ rps-tourney-deployment/deploy-to-justdavis.sh
@@ -4,8 +4,11 @@ scriptDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pom=$scriptDir/pom.xml
 
 # Specify the rps-tourney-* version to deploy as the first argument, e.g. 2.0.0-SNAPSHOT.
-# If not specified, it will be left out of he command, and the POM will default to the
+# If not specified, it will be left out of the command, and the POM will default to the
 # project's current version.
+# Please note that, for some reason, Cargo will only deploy artifacts that have been 
+# deployed to Nexus (via `mvn clean deploy`); artifacts in the local repository do not
+# seem to be used.
 versionArg=""
 if [[ -n $1 ]]
   then
