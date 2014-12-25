debeaaf4ffa472b46bba19b51d946cca31fc5c76
Karl M. Davis <karl@justdavis.com>
2014-12-25 16:48:01 -0500

    Resolves #66: Fix an error in the home page display.
    Added the resource string that was missing, and added an IT case to
    catch the problem in the future.

--- rps-tourney-webapp/src/test/java/com/justdavis/karl/rpstourney/webapp/home/HomeIT.java
+++ rps-tourney-webapp/src/test/java/com/justdavis/karl/rpstourney/webapp/home/HomeIT.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
 
+import com.justdavis.karl.rpstourney.service.api.game.State;
 import com.justdavis.karl.rpstourney.webapp.ITUtils;
 
 /**
@@ -53,6 +54,49 @@ public final class HomeIT {
 		} finally {
 			if (driver != null)
 				driver.quit();
+		}
+	}
+
+	/**
+	 * Ensures that the home page doesn't throw errors when it displays a game
+	 * at the {@link State#WAITING_FOR_FIRST_THROW} step. This is a regression
+	 * test case for <a
+	 * href="https://github.com/karlmdavis/rps-tourney/issues/66">Error on home
+	 * page after new game has been joined:
+	 * "No message found under code 'home.games.game.state.WAITING_FOR_FIRST_THROW'..."
+	 * </a>.
+	 */
+	@Test
+	public void home_withGameBeforeFirstThrow() {
+		WebDriver player1Driver = null;
+		WebDriver player2Driver = null;
+		try {
+
+			// Create the Selenium drivers for each player.
+			player1Driver = new HtmlUnitDriver(false);
+			player2Driver = new HtmlUnitDriver(false);
+
+			// Player 1: Create a game.
+			player1Driver.get(ITUtils.buildWebAppUrl("game/"));
+			String gameId = player1Driver.getCurrentUrl().substring(
+					player1Driver.getCurrentUrl().lastIndexOf('/') + 1);
+
+			// Player 2: Join the game.
+			player2Driver.get(ITUtils.buildWebAppUrl("game/" + gameId));
+			player2Driver.findElement(By.id("join-game")).click();
+
+			// Player 1: Go to the home page.
+			player1Driver.get(ITUtils.buildWebAppUrl("/"));
+
+			// Player 1: Spot-check one of the page's elements to ensure it's
+			// present.
+			Assert.assertNotNull(player1Driver.getPageSource(),
+					player1Driver.findElement(By.id("player-games")));
+		} finally {
+			if (player1Driver != null)
+				player1Driver.quit();
+			if (player2Driver != null)
+				player2Driver.quit();
 		}
 	}
 }
