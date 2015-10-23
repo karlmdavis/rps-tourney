455d5390dc17337ec3b1a901460234d9b38cdd3b
Karl M. Davis <karl@justdavis.com>
2015-10-22 20:39:37 -0400

    Resolves #102: Console script now works from other directories.

--- rps-tourney-console/src/main/assembly/rpstourney.sh
+++ rps-tourney-console/src/main/assembly/rpstourney.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#!/bin/bash
+
+# cd into the RPS bundle, to enable use of relative paths here.
+cd "${BASH_SOURCE%/*}" || exit
 
 # Run the JAR, passing all args to it.
 java -jar rps-tourney-console-${project.version}.jar "$@"
-
