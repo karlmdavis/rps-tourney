12ec197f77446a60fa4a6b0c590b3c1d27f82914
Karl M. Davis <karl@justdavis.com>
2015-10-29 21:14:15 -0400

    Issue #15: Refactored ConsoleGameDriver to reduce its complexity.

--- rps-tourney-console/src/main/java/com/justdavis/karl/rpstourney/app/console/ConsoleGameDriver.java
+++ rps-tourney-console/src/main/java/com/justdavis/karl/rpstourney/app/console/ConsoleGameDriver.java
@@ -7,6 +7,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Scanner;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.justdavis.karl.misc.exceptions.BadCodeMonkeyException;
 import com.justdavis.karl.rpstourney.api.GameSession;
 import com.justdavis.karl.rpstourney.app.console.i18n.IResourceBundleLoader;
@@ -24,6 +27,9 @@ import com.justdavis.karl.rpstourney.service.api.game.Throw;
  * (or something else connected to an input & output stream).
  */
 final class ConsoleGameDriver {
+	private static final Logger LOGGER = LoggerFactory
+			.getLogger(ConsoleGameDriver.class);
+
 	private final IResourceBundleLoader resourceBundleLoader;
 
 	/**
@@ -72,80 +78,7 @@ final class ConsoleGameDriver {
 		// Play rounds until the game is won.
 		try (Scanner scanner = new Scanner(in);) {
 			while ((game = getGame(gameBundle)).getWinner() == null) {
-				int currentRoundIndex = game.getRounds().size() - 1;
-				GameRound currentRound = game.getRounds()
-						.get(currentRoundIndex);
-
-				// Print out the round intro text.
-				out.println(String.format("\nRound %d!",
-						currentRound.getAdjustedRoundIndex() + 1));
-				out.println(String.format(" Score: %s",
-						determineScoreText(game)));
-
-				// Wait for the human player to select a valid move.
-				Throw humanMove = null;
-				while (humanMove == null) {
-					// Print out the instructions.
-					out.print(String.format(" Select your throw [%s,%s,%s]: ",
-							ThrowToken.ROCK.getToken(),
-							ThrowToken.PAPER.getToken(),
-							ThrowToken.SCISSORS.getToken()));
-
-					// Grab the next character entered by the player.
-					String tokenEnteredByHuman = scanner.next();
-
-					// Try to match that character against a throw.
-					ThrowToken selectedThrowToken = ThrowToken
-							.match(tokenEnteredByHuman);
-					if (selectedThrowToken != null)
-						humanMove = selectedThrowToken.getMove();
-					else
-						out.println(" Invalid throw.");
-
-					// Loop until a valid move is selected.
-				}
-
-				// Submit the human player's Throw.
-				gameBundle.getGameClient().submitThrow(gameBundle.getGameId(),
-						currentRoundIndex, humanMove);
-
-				// Wait for the opponent to make their Throw.
-				while (currentRound.getResult() == null) {
-					// Wait a bit, then refresh the game state to check again.
-					try {
-						Thread.sleep(500);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					game = getGame(gameBundle);
-					currentRound = game.getRounds().get(currentRoundIndex);
-				}
-
-				// Print out the round results.
-				PlayerRole[] playerRoles = game.getPlayerRoles(game
-						.getViewPlayer());
-				Throw yourThrow, opponentThrow;
-				if (playerRoles.length == 2) {
-					yourThrow = currentRound.getThrowForPlayer1();
-					opponentThrow = currentRound.getThrowForPlayer2();
-				} else if (playerRoles.length == 1
-						&& playerRoles[0].equals(PlayerRole.PLAYER_1)) {
-					yourThrow = currentRound.getThrowForPlayer1();
-					opponentThrow = currentRound.getThrowForPlayer2();
-				} else if (playerRoles.length == 1
-						&& playerRoles[0].equals(PlayerRole.PLAYER_2)) {
-					yourThrow = currentRound.getThrowForPlayer2();
-					opponentThrow = currentRound.getThrowForPlayer1();
-				} else {
-					throw new BadCodeMonkeyException();
-				}
-				out.println(String.format(" You threw %s, computer threw %s.",
-						ThrowToken.match(yourThrow).getFullDisplayText(),
-						ThrowToken.match(opponentThrow).getFullDisplayText()));
-				String winOrLossText = determineWonOrLostText(game,
-						currentRound);
-				out.println(String.format(" %s", winOrLossText));
+				playGameRound(gameBundle, out, scanner);
 			}
 		}
 
@@ -153,6 +86,123 @@ final class ConsoleGameDriver {
 		out.println();
 		out.println(String.format("Final Score: %s", determineScoreText(game)));
 		out.println(determineWonOrLostText(game));
+	}
+
+	/**
+	 * Plays a full, single round in the specified game.
+	 * 
+	 * @param gameBundle
+	 *            the {@link GameBundle} containing the game and the services
+	 *            needed to play through it
+	 * @param out
+	 *            the {@link PrintStream} to display the game on
+	 * @param scanner
+	 *            the {@link Scanner} to read the player's input from
+	 */
+	private static void playGameRound(GameBundle gameBundle, PrintStream out,
+			Scanner scanner) {
+		GameView game = getGame(gameBundle);
+
+		int currentRoundIndex = game.getRounds().size() - 1;
+		GameRound currentRound = game.getRounds().get(currentRoundIndex);
+
+		// Print out the round intro text.
+		out.println(String.format("\nRound %d!",
+				currentRound.getAdjustedRoundIndex() + 1));
+		out.println(String.format(" Score: %s", determineScoreText(game)));
+
+		// Wait for the human player to select a valid move and submit it.
+		Throw humanMove = requestHumanMove(out, scanner);
+		gameBundle.getGameClient().submitThrow(gameBundle.getGameId(),
+				currentRoundIndex, humanMove);
+
+		// Wait for the opponent to make their Throw.
+		waitForOpponentThrow(gameBundle, currentRoundIndex);
+
+		// Print out the round results.
+		game = getGame(gameBundle);
+		currentRound = game.getRounds().get(currentRoundIndex);
+		PlayerRole[] playerRoles = game.getPlayerRoles(game.getViewPlayer());
+		Throw yourThrow, opponentThrow;
+		if (playerRoles.length == 2) {
+			yourThrow = currentRound.getThrowForPlayer1();
+			opponentThrow = currentRound.getThrowForPlayer2();
+		} else if (playerRoles.length == 1
+				&& playerRoles[0].equals(PlayerRole.PLAYER_1)) {
+			yourThrow = currentRound.getThrowForPlayer1();
+			opponentThrow = currentRound.getThrowForPlayer2();
+		} else if (playerRoles.length == 1
+				&& playerRoles[0].equals(PlayerRole.PLAYER_2)) {
+			yourThrow = currentRound.getThrowForPlayer2();
+			opponentThrow = currentRound.getThrowForPlayer1();
+		} else {
+			throw new BadCodeMonkeyException();
+		}
+		out.println(String.format(" You threw %s, computer threw %s.",
+				ThrowToken.match(yourThrow).getFullDisplayText(), ThrowToken
+						.match(opponentThrow).getFullDisplayText()));
+		String winOrLossText = determineWonOrLostText(game, currentRound);
+		out.println(String.format(" %s", winOrLossText));
+	}
+
+	/**
+	 * @param out
+	 *            the {@link PrintStream} to display the game on
+	 * @param scanner
+	 *            the {@link Scanner} to read the player's input from
+	 * @return the {@link Throw} selected by the human player, that they would
+	 *         like to next make
+	 */
+	private static Throw requestHumanMove(PrintStream out, Scanner scanner) {
+		Throw humanMove = null;
+		while (humanMove == null) {
+			// Print out the instructions.
+			out.print(String.format(" Select your throw [%s,%s,%s]: ",
+					ThrowToken.ROCK.getToken(), ThrowToken.PAPER.getToken(),
+					ThrowToken.SCISSORS.getToken()));
+
+			// Grab the next character entered by the player.
+			String tokenEnteredByHuman = scanner.next();
+
+			// Try to match that character against a throw.
+			ThrowToken selectedThrowToken = ThrowToken
+					.match(tokenEnteredByHuman);
+			if (selectedThrowToken != null)
+				humanMove = selectedThrowToken.getMove();
+			else
+				out.println(" Invalid throw.");
+
+			// Loop until a valid move is selected.
+		}
+		return humanMove;
+	}
+
+	/**
+	 * @param gameBundle
+	 *            the {@link GameBundle} containing the game and the services
+	 *            needed to play through it
+	 * @param roundIndex
+	 *            the {@link GameRound#getRoundIndex()} of the game round to
+	 *            wait for the opponent to make a move in
+	 * @return an updated {@link GameView} instance, wherein the opponent has
+	 *         now made their move for the specified round
+	 */
+	private static void waitForOpponentThrow(GameBundle gameBundle,
+			int roundIndex) {
+		GameView game = getGame(gameBundle);
+		GameRound round = game.getRounds().get(roundIndex);
+
+		while (round.getResult() == null) {
+			// Wait a bit, then refresh the game state to check again.
+			try {
+				Thread.sleep(500);
+			} catch (InterruptedException e) {
+				// Nothing in this app uses interrupts; safe to log and ignore.
+				LOGGER.warn("Unexpected interrupt.", e);
+			}
+			game = getGame(gameBundle);
+			round = game.getRounds().get(roundIndex);
+		}
 	}
 
 	/**
